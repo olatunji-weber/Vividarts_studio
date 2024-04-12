a4cea74ddedc65fe341556eeefc123b9c3643ccd
Olatunji Olayinka <olatunji.weber@gmail.com>
2024-04-12 11:34:14 +0200

    New access Credentials put in environment variables

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ AWS_ACCESS_KEY_ID = os.environ.get("aws_key_id")
 AWS_SECRET_ACCESS_KEY = os.environ.get("aws_secret_key")
 S3_BUCKET_NAME = 'vividbucket-beanstalk2'
 
-s3 = boto3.client(S3_BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,
+                  aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
 
 @app.route('/')
@@ -20,7 +21,7 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
-    s3.upload_fileobj(file, 'vividbucket-beanstalk2', file.filename)
+    s3.upload_fileobj(file, S3_BUCKET_NAME, file.filename)
     return 'File uploaded successfully'
 
 
